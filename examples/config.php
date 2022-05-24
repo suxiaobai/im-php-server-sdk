4a21463c6faa17ec67e5f17ce7bf530176b912b8
maniac <maniacflow.dev@gmail.com>
2022-05-24 10:45:03 +0800

    Initial commit

---
+++ examples/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'easemob' => array(
+        'app_key' => 'appKey',
+        'client_id' => 'Client ID',
+        'client_secret' => 'ClientSecret',
+        'api_uri' => '',    // REST 域名
+    ),
+    'agora' => array(
+        'app_key' => 'appKey',
+        'app_id' => 'App ID',
+        'app_certificate' => 'App Certificate',
+        'expire_time' => 900,
+    ),
+);
